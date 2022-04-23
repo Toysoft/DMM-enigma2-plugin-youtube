5214a5306b46f1f8c3940de4e293e23f72d76cfb
reichi <reichi@opendreambox.org>
2022-04-23 12:23:01 +0200

    run 'onFirstExecBegin', delay console command for a bit because there is some unsolved timing issue related to rcu lock/unlock

--- src/YoutubeBrowser.py
+++ src/YoutubeBrowser.py
@@ -4,15 +4,19 @@ from Screens.Screen import Screen
 from Screens.ServiceStopScreen import ServiceStopScreen
 from Tools.Log import Log
 
-class YoutubeBrowser(Screen, ServiceStopScreen):
+from twisted.internet import reactor
+
+class YoutubeBrowser(Screen, ServiceStopScreen):#
+	skin = """<screen name="YoutubeBrowser" position="0,0" size="100%,100%" background="#000000" flags="wfNoBorder"></screen>"""
 	def __init__(self, session):
-		Screen.__init__(self, session)
+		Screen.__init__(self, session, windowTitle="YouTube")
 		ServiceStopScreen.__init__(self)
 
 		self.__container = eConsoleAppContainer()
 		self.__appClosed_conn = self.__container.appClosed.connect(self.__runFinished)
+		self.__container_conn = self.__container.dataAvail.connect(self.__consoleData)
 		self.stopService()
-		self.__run()
+		self.onFirstExecBegin.append(self.__run)
 
 	def __run(self):
 		#eDBoxLCD.getInstance().lock()
@@ -20,8 +24,10 @@ class YoutubeBrowser(Screen, ServiceStopScreen):
 		fbClass.getInstance().lock()
 		cmd = "export LANG=" + language.getLanguage() + ".UTF-8;/usr/bin/dreamium"
 		Log.w(cmd)
-		self.__container.execute(cmd)
+		reactor.callLater(3, self.__container.execute, cmd)
 
+	def __consoleData(self, data):
+		Log.i(data)
 
 	def __runFinished(self, retval=1):
 		#Log.w(retval)
