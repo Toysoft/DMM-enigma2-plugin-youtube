f8a7d8ca7417809392b48921bb75592d26820faf
reichi <reichi@opendreambox.org>
2022-09-27 11:06:40 +0200

    lock/unlock is now fixed in enigma2, so lock lcd and fb, again. Improve the whole code and visuals a bit

--- src/YoutubeBrowser.py
+++ src/YoutubeBrowser.py
@@ -1,4 +1,5 @@
 from enigma import eConsoleAppContainer, eDBoxLCD, eRCInput, fbClass
+from Components.Label import Label
 from Components.Language import language
 from Screens.Screen import Screen
 from Screens.ServiceStopScreen import ServiceStopScreen
@@ -7,31 +8,40 @@ from Tools.Log import Log
 from twisted.internet import reactor
 
 class YoutubeBrowser(Screen, ServiceStopScreen):#
-	skin = """<screen name="YoutubeBrowser" position="0,0" size="100%,100%" background="#000000" flags="wfNoBorder"></screen>"""
+	skin = """<screen name="YoutubeBrowser" position="0,0" size="100%,100%" backgroundColor="#000000" flags="wfNoBorder">
+		<widget name="state" position="0,0" size="100%,100%" halign="center" valign="center" foregroundColor="#FFFFFF" backgroundColor="#000000" font="Regular;128"/>
+	</screen>"""
 	def __init__(self, session):
 		Screen.__init__(self, session, windowTitle="YouTube")
 		ServiceStopScreen.__init__(self)
 
+		self["state"] = Label(_("Loading YouTube ..."))
 		self.__container = eConsoleAppContainer()
 		self.__appClosed_conn = self.__container.appClosed.connect(self.__runFinished)
 		self.__container_conn = self.__container.dataAvail.connect(self.__consoleData)
 		self.stopService()
-		self.onFirstExecBegin.append(self.__run)
+		self._shouldRun = True
+		self.onShown.append(self.__run)
 
 	def __run(self):
-		#eDBoxLCD.getInstance().lock()
+		if not self._shouldRun:
+			return
+		self._shouldRun = False
+		reactor.callLater(.5, self.__doRun)
+
+	def __doRun(self):
 		eRCInput.getInstance().lock()
+		eDBoxLCD.getInstance().lock()
 		fbClass.getInstance().lock()
 		cmd = "export LANG=" + language.getLanguage() + ".UTF-8;/usr/bin/dreamium"
 		Log.w(cmd)
-		reactor.callLater(3, self.__container.execute, cmd)
+		reactor.callLater(1, self.__container.execute(cmd))
 
 	def __consoleData(self, data):
 		Log.i(data)
 
 	def __runFinished(self, retval=1):
-		#Log.w(retval)
-		#eDBoxLCD.getInstance().unlock()
 		eRCInput.getInstance().unlock()
+		eDBoxLCD.getInstance().unlock()
 		fbClass.getInstance().unlock()
 		self.close()
